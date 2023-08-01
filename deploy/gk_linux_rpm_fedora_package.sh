1fc1c09180ff1f938e48afef93662d827aacd0a0
blackcross86 <74768319+blackcross86@users.noreply.github.com>
2023-08-01 11:50:49 +0300

    Update gk_linux_rpm_fedora_package.sh
    fix the packaging script

--- deploy/gk_linux_rpm_fedora_package.sh
+++ deploy/gk_linux_rpm_fedora_package.sh
@@ -3,18 +3,35 @@
 APP_VER="$1"
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "Install the build dependencies"
+echo "dnf in /usr/bin/rpmdev-setuptree dotnet-sdk-6.0"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
 rm -rf ~/rpmbuild/
-mkdir -p ~/rpmbuild/BUILD
-mkdir -p ~/rpmbuild/RPMS
-mkdir -p ~/rpmbuild/SOURCES
-mkdir -p ~/rpmbuild/SPECS
-mkdir -p ~/rpmbuild/SRPMS
+rpmdev-setuptree
 
-tar -zcf ~/rpmbuild/SOURCES/gedkeeper-$APP_VER.tar.gz -T "$DIR/rpm/gk_files.txt"
+tar -zcf ~/rpmbuild/SOURCES/gedkeeper-$APP_VER.tar.gz \
+    	./gk_run.sh \
+	./gedkeeper.png \
+	./gedkeeper.desktop \
+	./application-x-gedkeeper.xml \
+	./gedkeeper.appdata.xml \
+	../bin \
+	../LICENSE \
+	../locales/ \
+	../plugins/ \
+	../scripts/ \
+	../samples/ \
+	../themes/ \
+	../externals/resources.yaml
+
 cp "$DIR/rpm/gedkeeper.spec" ~/rpmbuild/SPECS/gedkeeper.spec
-cd ~/rpmbuild/SPECS/
 
+pushd ~/rpmbuild/SPECS/
 # build from binary
 rpmbuild -bb gedkeeper.spec
+popd
 
 cd "$DIR"
+
